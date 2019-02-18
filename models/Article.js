69c054bb8bc459b3daf536d948a1e6c1a18c1cbf
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-02-17 17:58:25 -0800

    [add] Models for Data

---
+++ models/Article.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+// Save a reference to the Schema constructor
+const Schema = mongoose.Schema
+
+// Schema Constructor
+const ArticleSchema = new Schema({
+    // 'title' is required and of type String
+    title: {
+        type: String,
+        required: true
+    },
+    // 'link' is required and of type String
+    link: {
+        type: String,
+        required: true
+    }
+})
+
+// Creates a model from the Schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema)
+
+// Export the Article Model
+module.exports = Article
--- server.js
+++ server.js
@@ -5,11 +5,35 @@ const mongoose = require("mongoose")
 
 // Scrapping Tools
 const axios = require("axios")
-const cherrio = require("cheerio");
+const cherrio = require("cheerio")
 
 // Initalize Express
 const app = express()
 const port = 3000
+
+// Configure middleware
+
+// Use morgan logger for logging results
+app.use(logger("dev"))
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }))
+// Use express.static to serve the public folder as a static directory
+// TODO: Create a public folder for a directory
+// app.use(express.static("public"))
+
+// Connect to the MongoDB
+mongoose.connect("mongodb://localhost/disney-pin-news-db", { useNewUrlParser: true }, console.log("Connected successfully to database"))
+
+// A GET route for scraping the DisneyPinBlogs website
+app.get("/scrape", function(req, res) {
+    // Grab the bots of html with axios
+    axios.get("http://disneypinsblog.com/blog/").then(function(response){
+        // Then, load that into cheerio and save it to $ for shorthand selector
+        const $ = cheerio.load(response.data)
+
+        
+    })
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
