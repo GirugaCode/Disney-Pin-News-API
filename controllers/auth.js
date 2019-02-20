2b6b8b6b86725b2b55306765607052ceb68eda69
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-02-19 21:44:14 -0800

    [add] JWT token excercise

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const User = require("../models/user")
+
+module.exports = (app) => {
+    //SIGN UP FORM
+    app.get("/sign-up", (req, res) => {
+        res.render('sign-up')
+    })
+
+    //SIGN UP POST
+    app.post("/sign-up", (req, res) => {
+        // Create User
+        const newUser = new User(req.body);
+    
+        newUser
+          .save(function (err){
+            if (err) console.log(err)
+            const token = jwt.sign({ _id: newUser._id }, 'shhhhhhared-secret')
+            res.json({"token": token})
+          })
+      });
+}
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const logger = require("morgan")
 const mongoose = require("mongoose")
+const exphbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
+const jwt = require('express-jwt')
+
+
 
 // Scrapping Tools
 const axios = require("axios")
@@ -11,6 +16,11 @@ const cherrio = require("cheerio")
 const app = express()
 const port = 3000
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
 // Configure middleware
 
 // Use morgan logger for logging results
@@ -19,7 +29,23 @@ app.use(logger("dev"))
 app.use(bodyParser.urlencoded({ extended: true }))
 // Use express.static to serve the public folder as a static directory
 // TODO: Create a public folder for a directory
-// app.use(express.static("public"))
+app.use(express.static("public"))
+
+app.use(cookieParser());
+app.use(
+  jwt({
+    secret: "shhhhhhared-secret",
+    getToken: function fromHeaderOrCookie(req) {
+      //fromHeaderOrQuerystring
+      if (req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer") {
+        return req.headers.authorization.split(" ")[1];
+      } else if (req.cookies && req.cookies.token) {
+        return req.cookies.token;
+      }
+      return null;
+    }
+  }).unless({ path: ["/", "/login", "/sign-up"] })
+);
 
 // Connect to the MongoDB
 mongoose.connect("mongodb://localhost/disney-pin-news-db", { useNewUrlParser: true }, console.log("Connected successfully to database"))
@@ -35,6 +61,11 @@ app.get("/scrape", function(req, res) {
     })
 })
 
+// Exporting Auth Controller
+require('./controllers/auth.js')(app)
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app
