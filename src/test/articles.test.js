480e74d0218ec671af2d33702b028edf6cefdb5e
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-03-06 18:26:42 -0800

    [deploy] to heroku

--- src/test/articles.test.js
+++ src/test/articles.test.js
@@ -1,8 +1,7 @@
+/* eslint-disable no-undef */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../server');
-
-const should = chai.should();
 
 chai.use(chaiHttp);
 
