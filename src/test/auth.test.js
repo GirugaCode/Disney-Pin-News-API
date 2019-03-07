a2ebfdbe993db802be4ff859453c15328eabf60a
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-03-06 18:26:10 -0800

    [deploy] to heroku

--- src/test/auth.test.js
+++ src/test/auth.test.js
@@ -3,7 +3,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('./../server');
 
-const should = chai.should();
 chai.use(chaiHttp);
 
 // Agent that will keep track of our cookies
@@ -27,7 +26,7 @@ describe('User', () => {
         .post('/sign-up')
         .send({ username: 'testone', password: 'password' })
         .end((err, res) => {
-          console.log(res.body);
+          // console.log(res.body);
           res.should.have.status(200);
           agent.should.have.cookie('nToken');
           done();
